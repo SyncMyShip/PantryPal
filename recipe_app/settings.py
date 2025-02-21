eeb2d6f7357d5c564871681bd3e95e6bcdd02d71
SyncMyShip <77575480+SyncMyShip@users.noreply.github.com>
2025-02-20 23:58:17 -0500

    Update settings.py

--- recipe_app/settings.py
+++ recipe_app/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 import os
 from django.core.exceptions import ImproperlyConfigured
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -153,7 +154,6 @@ LOGIN_URL='/login/'
 
 
 # Heroku: Update db configuration from $DATABASE_URL
-import dj_database_url
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
